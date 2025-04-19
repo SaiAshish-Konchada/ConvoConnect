d73ce2803e96bf07605a8dafb4eea2ffd116da99
Sai Ashish <saiashishkonchada@gmail.com>
2025-04-19 15:57:55 +0530

    Fix Link

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -14,6 +14,7 @@ import AvatarOpenEyes from "../../assets/avatar-open.png";
 import AvatarClosedEyes from "../../assets/avatar-closed.png";
 import AvatarCheeky from "../../assets/avatar-cheeky.png";
 import AvatarSuccess from "../../assets/avatar-success.png";
+import { Link } from "react-router-dom"; 
 
 /**
  * Validation utilities (can be moved to a hook or util file)
@@ -31,7 +32,7 @@ const LoginForm = ({ onSubmit, isLoading }) => {
 
   const allFieldsValid = useMemo(
     () => validateEmail(formData.email) && validatePassword(formData.password),
-    [formData],
+    [formData]
   );
 
   const progressValue = useMemo(() => {
@@ -198,9 +199,9 @@ const LoginForm = ({ onSubmit, isLoading }) => {
       <div className="text-center">
         <p className="text-base-content/60">
           Ready to join the club?{" "}
-          <a href="/signup" className="link link-primary">
+          <Link to="/signup" className="link link-primary">
             Create account
-          </a>
+          </Link>
         </p>
       </div>
     </div>
